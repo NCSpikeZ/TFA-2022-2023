0af9d48884cf65e77664cfdf56bf062509c6a845
NCSpikeZ <nicolas.coopman@student.heaj.be>
2023-06-02 00:45:55 +0200

    continuation première page slide ect

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,37 @@ function initNav() {
     nav.classList.toggle('show');
   });
 }
+
+const counterElement = document.getElementById("counter");
+const targetCount = 660000;
+let currentCount = 0;
+
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+function updateCounter() {
+  counterElement.innerText = currentCount.toLocaleString();
+
+  if (currentCount < targetCount) {
+    currentCount += 1000;
+    setTimeout(updateCounter, 1);
+  } else {
+    currentCount = targetCount;
+  }
+}
+
+function updateCounterOnScroll() {
+  if (isElementInViewport(counterElement)) {
+    updateCounter();
+    window.removeEventListener('scroll', updateCounterOnScroll);
+  }
+}
+
+window.addEventListener('scroll', updateCounterOnScroll);

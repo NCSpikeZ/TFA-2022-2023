0b36751082a30fc3fe5645a7bd3b132d0e95dbaf
NCSpikeZ <nicolas.coopman@student.heaj.be>
2023-06-02 20:56:45 +0200

    fin version mobile landing v1 (important)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,3 +43,63 @@ function updateCounterOnScroll() {
 }
 
 window.addEventListener('scroll', updateCounterOnScroll);
+
+//Slider
+const btnPrev = document.querySelector(".slider__btn--prev"),
+      btnNext = document.querySelector(".slider__btn--next");
+
+btnNext.addEventListener("click", next);
+btnPrev.addEventListener("click", prev);
+
+
+//Touches clavier
+document.addEventListener("keydown", function(e){
+    if(e.code == "ArrowLeft"){
+        prev();
+    }else if(e.code == "ArrowRight"){
+        next();
+    }
+});
+
+//Hammer
+const slider = document.querySelector(".slider"),
+    hammerSlider = new Hammer(slider);
+
+hammerSlider.on("swipeleft", next);
+hammerSlider.on("swiperight", prev);
+
+function next (){
+    let elShow = document.querySelector(".slider__el--show"),
+        elNext = elShow.nextElementSibling;
+
+        elShow.classList.remove("slider__el--show");
+
+        if(elNext){
+            elNext.classList.add("slider__el--show");
+        }else{
+            let elFirst = elShow.parentNode.firstElementChild;
+            elFirst.classList.add("slider__el--show");
+        }
+
+}
+
+
+function prev(){
+    let elShow = document.querySelector(".slider__el--show"),
+        elPrev = elShow.previousElementSibling;
+
+        elShow.classList.remove("slider__el--show");
+
+        if(elPrev){
+            elPrev.classList.add("slider__el--show");
+        }else{
+            let elLast = elShow.parentNode.lastElementChild;
+            elLast.classList.add("slider__el--show");
+        }
+
+}
+
+/* Copyright */
+let year = new Date().getFullYear();
+let date = document.querySelector("#date");
+date.innerHTML = "©SpikeZ"+ String(year);
